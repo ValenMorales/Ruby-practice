fe751b7cd6572082e553408f6124931b3b177adb
ValenMorales <valentina.morales@kommit.co>
2024-09-16 15:12:43 -0500

    improvement of calculator logic adding switch case to evaluate the selected operation and while loop to asking more numbers and operations

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,29 @@
+require_relative "methods"
+
 puts "Welcome to the basic calculator"
-puts "Please enter the first number"
-num1 = gets.chomp.to_f
 
-puts "Please enter the second number"
-num2 = gets.chomp.to_f
+loop do
+    puts "Please enter the first number (or type 'exit' to quit)"
+    input_num1 = gets.chomp
+    break if input_num1.downcase == "exit"
+    num1 = input_num1.to_f
 
-result = num1 + num2
+    puts "Please enter the second number"
+    num2 = gets.chomp.to_f
 
-puts "#{num1} + #{num2} = #{result}"
+    puts "Please choose an operation: add, subtract, multiply, divide, power, modulo, logarithm, square"
+    operation = gets.chomp.downcase
+    break if operation == "exit"
 
+    if operation == "square_root"
+    result = evaluate_selection(operation, num1)
+    else
+    result = evaluate_selection(operation, num1, num2)
+    end
+
+    puts "The result is: #{result}"
+end 
+
+puts "Bye!"
+
+    
