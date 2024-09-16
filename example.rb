7ef8e55ad3e94e3b8acb3652ef39a5a6938ef4fc
ValenMorales <valentina.morales@kommit.co>
2024-09-16 12:56:49 -0500

    understanding fundamentals of input and output of data

---
+++ example.rb
@@ -0,0 +1,11 @@
+puts "Welcome to the basic calculator"
+puts "Please enter the first number"
+num1 = gets.chomp.to_f
+
+puts "Please enter the second number"
+num2 = gets.chomp.to_f
+
+result = num1 + num2
+
+puts "#{num1} + #{num2} = #{result}"
+
